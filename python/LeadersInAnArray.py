c27494291c848f65e8da3fa9f88f297d7ae5e13c
Chris Nasser <4016056+Nasser-Christopher@users.noreply.github.com>
2024-03-20 12:55:12 -0700

    Submission Attempt 1

--- python/LeadersInAnArray.py
+++ python/LeadersInAnArray.py
@@ -6,13 +6,21 @@ class Solution:
     #Back-end complete function Template for Python 3
     
     #Function to find the leaders in the array.
-    
     # A is the Array, N is Length of Array
     def leaders(self, A, N):
         # if A[x] >= A[x+1] + A[x+2] + ... + A[N-1]
         # then add A[x] to the list of leaders
+        
+        # Create a list to store the leaders, and a variable to store the local max
+        # slice the array from A[x] to A[N-1]
+        # if A[x] >= max(slice) - A[x], then add A[x] to the list of leaders
         answer = [None]
+        localMax = None
+        arraySlice = None
         
-        
-        #Code here
-        pass
+        for i in range(N):
+            arraySlice = A[i:N]
+            localMax = max(arraySlice) - A[i]
+            if A[i] >= localMax:
+                answer.append(A[i])
+        return answer
