52326d43ae43c27f3d8d03f15dbab20780ae5296
Chris Nasser <4016056+Nasser-Christopher@users.noreply.github.com>
2024-03-20 12:38:01 -0700

    Leaders in an array

---
+++ python/LeadersInAnArray.py
@@ -0,0 +1,18 @@
+# Given an array A of positive integers. Your task is to find the leaders in the array.
+# An element of array is a leader if it is **greater than or equal to all the elements to its right side**.
+# The rightmost element is always a leader.
+
+class Solution:
+    #Back-end complete function Template for Python 3
+    
+    #Function to find the leaders in the array.
+    
+    # A is the Array, N is Length of Array
+    def leaders(self, A, N):
+        # if A[x] >= A[x+1] + A[x+2] + ... + A[N-1]
+        # then add A[x] to the list of leaders
+        answer = [None]
+        
+        
+        #Code here
+        pass
