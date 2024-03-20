e0a9e65f86962cecbc19d2da9ba068726edca219
Chris Nasser <4016056+Nasser-Christopher@users.noreply.github.com>
2024-03-20 13:28:57 -0700

    Submission 4 (Not a Summation)

--- python/LeadersInAnArray.py
+++ python/LeadersInAnArray.py
@@ -8,19 +8,28 @@ class Solution:
     #Function to find the leaders in the array.
     # A is the Array, N is Length of Array
     def leaders(self, A, N):
+        
+        
+        # Wrong Solution (Summation):
         # if A[x] >= A[x+1] + A[x+2] + ... + A[N-1]
         # then add A[x] to the list of leaders
+        # 1.Create a list to store the leaders, and a variable to store the local max
+        # 2.slice the array from A[x] to A[N-1]
+        # 3.if A[x] >= max(slice) - A[x], then add A[x] to the list of leaders
         
-        # Create a list to store the leaders, and a variable to store the local max
-        # slice the array from A[x] to A[N-1]
-        # if A[x] >= max(slice) - A[x], then add A[x] to the list of leaders
+        # Real Solution (Comparison):
+        # The value at A[x] must be greater than the INDIVIDUAL VALUES to its right, not the summation of said values.
         answer = []
         localMax = None
-        arraySlice = None
+        arrayWindow = None
         
         for i in range(N):
-            arraySlice = A[i:]
-            localMax = sum(arraySlice) - A[i]
+            arrayWindow = A[i:N]
+            localMax = max(arrayWindow)
             if A[i] >= localMax:
                 answer.append(A[i])
+                
         return answer
+            
+        
+        
