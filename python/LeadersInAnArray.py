bd99580eae0dcfa8c455cead9221102cadbbaa2e
Chris Nasser <4016056+Nasser-Christopher@users.noreply.github.com>
2024-03-20 14:36:05 -0700

    Final Submission (9 Attempts)

--- python/LeadersInAnArray.py
+++ python/LeadersInAnArray.py
@@ -19,17 +19,26 @@ class Solution:
         
         # Real Solution (Comparison):
         # The value at A[x] must be greater than the INDIVIDUAL VALUES to its right, not the summation of said values.
-        answer = []
-        localMax = None
-        arrayWindow = None
+        # NOTE: using a for loop with a max() function will be O(n^2) time complexity, which fails the time limit.
         
-        for i in range(N):
-            arrayWindow = A[i:N]
-            localMax = max(arrayWindow)
-            if A[i] >= localMax:
-                answer.append(A[i])
-                
-        return answer
+        # 1.Create a list to store the leaders, and a variable to store the current max
+        # 2.Start from the rightmost element, and add it to the list of leaders
+        # 3.Slice the array from A[N-2] to A[0]
+        # 4.if A[x] >= currentMax, then add A[x] to the list of leaders
+        # 5.Update currentMax to A[x]
+        # 6.Repeat steps 3-5 until the end of the array
+        # 7.Reverse the list of leaders and return it
+        answer = []
+        
+        currentMax = A[N-1]
+        answer.append(currentMax)
+        for i in range(N-2, -1, -1):
+            if A[i] >= currentMax:
+                currentMax = A[i]
+                answer.append(currentMax)
+        
+        return answer[::-1]
             
         
-        
+A = Solution()
+print(A.leaders([63,70,80,33,33,47,20], 7)) # [80, 47, 20]
