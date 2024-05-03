a73a4d914518595be76b82c380d60709f6694ec3
Chris Nasser <4016056+Nasser-Christopher@users.noreply.github.com>
2024-05-03 00:03:51 -0700

    Initial question setup

---
+++ python/MissingNumberInArray.py
@@ -0,0 +1,27 @@
+# Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+# Example 1:
+
+# Input:
+# N = 5
+# A[] = {1,2,3,5}
+# Output: 4
+
+# Your Task :
+# You don't need to read input or print anything.
+# Complete the function MissingNumber() that takes array and N as input parameters
+# and returns the value of the missing number.
+
+
+# Expected Time Complexity: O(N)
+# Expected Auxiliary Space: O(1)
+
+
+class Solution:
+    def missingNumber(self, array, n):
+        pass
+        # code here
+
+
+A = Solution()
+print(A.missingNumber([1, 2, 3, 5], 5))  # 4
